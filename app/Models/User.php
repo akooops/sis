28d5ccd10511b3b04a1a59ca4d170eee7bad0195
akooops <lyes24.azzi@gmail.com>
2025-05-13 10:55:22 +0100

    feat: users auth imlmented

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,32 +2,24 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\BaseModel;
+use App\Models\File;
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\UserRole;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\URL;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    
+    //Properties
+    protected $guarded = ['id'];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
@@ -42,4 +34,69 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected $appends = ['fullname', 'avatarUrl'];
+
+    //Relationships
+    public function avatar()
+    {
+        return $this->morphOne(File::class, 'model')->where('is_main', true);
+    }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'user_roles', 'user_id', 'role_id')
+            ->using(UserRole::class)
+            ->withTimestamps();
+    }
+    
+    //Scopes
+
+    //Accessors & Mutators
+    public function getFullnameAttribute()
+    {
+        return "{$this->firstname} {$this->lastname}";
+    }
+
+    public function getAvatarUrlAttribute()
+    {
+        // If user has a profile image, return it
+        return ($this->avatar) ? $this->avatar->url : URL::to('assets/images/default-avatar.jpg');
+    }
+
+    //Custom Methods
+    public function hasRole($role)
+    {
+        return $this->roles()->where('id', $role)
+            ->orWhere('name', $role)
+            ->exists();
+    }
+
+    public function hasPermission($permission, $module = null)
+    {
+        $query = $this->roles()
+            ->join('role_permissions', 'roles.id', '=', 'role_permissions.role_id')
+            ->join('permissions', 'role_permissions.permission_id', '=', 'permissions.id')
+            ->where('permissions.name', $permission);
+    
+        if ($module) {
+            $query->where('permissions.module_id', $module);
+        }
+            
+        return $query->exists();
+    }
+    
+    public function hasAnyPermission($permissions, $module = null)
+    {
+        $query = $this->roles()
+            ->join('role_permissions', 'roles.id', '=', 'role_permissions.role_id')
+            ->join('permissions', 'role_permissions.permission_id', '=', 'permissions.id')
+            ->whereIn('permissions.name', (array) $permissions);
+
+        if ($module) {
+            $query->where('permissions.module_id', $module);
+        }
+
+        return $query->exists();
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('azure_ad_id')->nullable();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone', 20)->unique()->nullable();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
