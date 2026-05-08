30161464da6fb14e640cbd8582799a8c2506ea5b
Ilyes Azzi <ilyes24.azzi@gmail.com>
2026-05-08 20:34:24 +0100

    fix: fixed pathway

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -107,7 +107,7 @@
                             </p>
                         </div>
 
-                        <a href="{{$pathwayAmericanUrl && !empty($pathwayAmericanUrl->value) ? $pathwayAmericanUrl : '#'}}" class="btn py-1 rounded">
+                        <a href="{{$pathwayAmericanUrl && !empty($pathwayAmericanUrl->value) ? $pathwayAmericanUrl->value : '#'}}" class="btn py-1 rounded">
                             <span class="mx-4">{{getLanguageKeyLocalTranslation('index_page_pathway_american_cta')}}</span>
                                 <i class="uil uil-arrow-right"></i>
                         </a>
@@ -132,7 +132,7 @@
                             </p>
                         </div>
 
-                        <a href="{{$pathwayBritishUrl && !empty($pathwayBritishUrl->value) ? $pathwayBritishUrl : '#'}}" class="btn py-1 rounded">
+                        <a href="{{$pathwayBritishUrl && !empty($pathwayBritishUrl->value) ? $pathwayBritishUrl->value : '#'}}" class="btn py-1 rounded">
                             <span class="mx-4">{{getLanguageKeyLocalTranslation('index_page_pathway_british_cta')}}</span>
                             <i class="uil uil-arrow-right"></i>
                         </a>
